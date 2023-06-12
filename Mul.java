85ead3def40836d04817157a7c7483d4b3ab23ec
Akshitha Chiyanoor <akshitha.chiyanoor@netenrich.com>
2023-06-12 14:34:44 +0530

    commited new file

---
+++ Mul.java
@@ -0,0 +1,9 @@
+public class Mul {
+    public static void main(String args[])
+    {
+        int num1=11;
+        int num2=6;
+        System.out.println("Multiplication:"+num1*num2);
+    }
+    
+}

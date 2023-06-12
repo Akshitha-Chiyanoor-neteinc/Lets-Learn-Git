69c29cebf1baa90332966e244da267d53ce3db2a
Akshitha Chiyanoor <akshitha.chiyanoor@netenrich.com>
2023-06-12 17:13:07 +0530

    my new division file

---
+++ division.java
@@ -0,0 +1,11 @@
+public class division {
+
+    public static void main(String args[])
+    {
+        int num1=22;
+        int num2=11;
+        System.out.println("Division:"+num1/num2);
+       // System.out.println("hello there folks i m here");
+
+    }
+}

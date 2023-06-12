13a5be4a227926668cb116331f572283c4d74b32
Akshitha Chiyanoor <akshitha.chiyanoor@netenrich.com>
2023-06-12 15:08:00 +0530

    my sout commit is done!

--- Mul.java
+++ Mul.java
@@ -5,7 +5,7 @@ public class Mul {
         int num2=6;
         System.out.println("Multiplication:"+num1*num2);
         System.out.println("hello there folks i m here");
-
+        System.out.println("my name is akshitha nair");
     }
     
 }
